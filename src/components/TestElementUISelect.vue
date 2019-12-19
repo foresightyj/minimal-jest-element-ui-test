c8c68cb8206137eb09596355584b5857fb3fcc25
foresightyj <imyuanjian@gmail.com>
2019-12-19 13:22:05 +0800

    minimal test case for element ui select

---
+++ src/components/TestElementUISelect.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <span>element ui select</span>
+    <el-select v-model="value" placeholder="请选择">
+      <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value"></el-option>
+    </el-select>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { Button, Select, Option } from "element-ui";
+export default Vue.extend({
+  components: {
+    [Select.name]: Select,
+    [Option.name]: Option
+  },
+  data() {
+    return {
+      options: [{
+        value: '选项1',
+        label: '黄金糕'
+      }, {
+        value: '选项2',
+        label: '双皮奶'
+      }],
+      value: ''
+    };
+  },
+});
+</script>
--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,12 +1,16 @@
-import { shallowMount } from '@vue/test-utils';
-import HelloWorld from '@/components/HelloWorld.vue';
+import { mount, shallowMount } from '@vue/test-utils';
+import TestElementUISelect from '@/components/TestElementUISelect.vue';
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message';
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg },
+describe('TestElementUISelect.vue', () => {
+    it('shallowMount ok', () => {
+        const wrapper = shallowMount(TestElementUISelect, {
+        });
+        expect(wrapper.text()).toMatch("element ui select");
     });
-    expect(wrapper.text()).toMatch(msg);
-  });
+    it('mount not ok', () => {
+        const wrapper = mount(TestElementUISelect, {
+            attachToDocument: true
+        });
+        expect(wrapper.text()).toMatch("element ui select");
+    });
 });
