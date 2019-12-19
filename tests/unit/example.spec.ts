d4cd6f1b9ff9b589c1073cb271bfa2b82a874424
foresightyj <imyuanjian@gmail.com>
2019-12-19 14:17:25 +0800

    still has issues

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,15 +1,26 @@
-import { mount, shallowMount } from '@vue/test-utils';
+import { mount, shallowMount, config, createLocalVue } from '@vue/test-utils';
 import TestElementUISelect from '@/components/TestElementUISelect.vue';
+import ElementUI from 'element-ui';
+
+(config.stubs as any).transition = false;
+const localVue = createLocalVue();
+
+// this works https://github.com/ziyoung/vue-test-with-jest/blob/master/src/components/__tests__/hello-world.test.js
+// see https://github.com/ElemeFE/element/issues/12158
+// also https://github.com/vuejs/vue-test-utils/issues/829
 
 describe('TestElementUISelect.vue', () => {
     it('shallowMount ok', () => {
         const wrapper = shallowMount(TestElementUISelect, {
+            localVue,
         });
         expect(wrapper.text()).toMatch("element ui select");
     });
     it('mount not ok', () => {
         const wrapper = mount(TestElementUISelect, {
-            attachToDocument: true
+            localVue,
+            sync: false,
+            // attachToDocument: true,
         });
         expect(wrapper.text()).toMatch("element ui select");
     });
